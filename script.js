3247667e6339a7115ff49a199da4875c9e95c420
Maria Beatriz Leandro Da Silva <mariabeatriz@MacBook-Air-de-Maria.local>
2026-01-21 23:44:42 -0300

    Adicionando campo aluno/visitante e ajustes de layout

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     cards.forEach(card => cardObserver.observe(card));
 
 
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbyiWjzuLhjdLbC6Odhps_oD3pYBfpnFV04coXF8iHGWT6elz8y9XcABfs_1vLk-eva2/exec'; 
+    const scriptURL = "https://script.google.com/macros/s/AKfycbwAUx5a7DRrcpNFfTKlPlF6hjqig79k0TItxpKRIQNT6Bdjfz_dm_ybZWqiW3E3yVsR/exec";
     const form = document.getElementById('inscricaoForm');
     const btn = document.getElementById('btnEnviar');
     const modal = document.getElementById('modal');
